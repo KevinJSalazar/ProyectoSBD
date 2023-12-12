ed715a02eb12c7a0a0ca989f29382761ff8b008b
AlexDiez10 <ajdiez@espol.edu.ec>
2023-12-11 23:23:26 -0500

    10 de servicio, 10 de solicitud, 10 de comentario

--- petbacker.sql
+++ petbacker.sql
@@ -5,7 +5,7 @@ create table RefugioMascotas(
 	idRef int not null unique check(idRef > 0 AND idRef < 1000),
     email char(50) not null unique,
     descripcion char(250) not null,
-    foto blob not null,
+    foto char(250) not null,
     pagina_web char(50) not null,
     direccion char(50) not null,
     estado char(30) not null,
@@ -20,7 +20,7 @@ create table Usuario(
     nickname char(250) not null,
     contraseña char(50) not null,
     telefono char(20),
-    foto blob not null,
+    foto char(250) not null,
     red_social char(30),
     vales int not null,
     tarjeta int,
@@ -40,7 +40,7 @@ create table Donacion(
 );
 create table TipoServicio(
 	idTipoSer int not null unique check(idTipoSer > 0 AND idTipoSer < 10),
-    lugar_recogida date not null,
+    lugar_recogida char(250) not null,
     fecha_inicio date not null,
     numero_dias int not null check(numero_dias >= 1 AND numero_dias <= 365),
 	fecha_fin date not null,
@@ -92,6 +92,7 @@ create table Solicitud(
 	idSer int not null unique check(idSer > 0 AND idSer < 100000),
     idUsu int not null unique check(idUsu > 0 AND idUsu < 100000),
     idCui int not null unique check(idCui > 0 AND idCui < 100000),
+	constraint diferentes1 check(idUsu != idCui),
     fecha date not null,
     primary key(idSol, idSer, idUsu, idCui),
     foreign key(idSer) references Servicio(idSer),
@@ -103,6 +104,7 @@ create table Comentario(
 	idSer int not null unique check(idSer > 0 AND idSer < 100000),
     idUsu int not null unique check(idUsu > 0 AND idUsu < 100000),
     idCui int not null unique check(idCui > 0 AND idCui < 100000),
+	constraint diferentes2 check(idUsu != idCui),
     fecha date not null,
     valoracion double not null,
     contenido char(250) not null,
@@ -110,4 +112,39 @@ create table Comentario(
     foreign key(idSer) references Servicio(idSer),
     foreign key(idUsu) references Usuario(idUsu),
     foreign key(idCui) references Usuario(idUsu)
-)
+);
+
+use PetBacker;
+insert into Servicio values(60, 20, "Cuidado de Mascotas", "cuidado bonito", 25.50, Ecuador, Guayas, Guayaquil, 70); 
+insert into Servicio values(61, 21, "Cuidado de Mascotas", "cuidado bonito", 25.50, Ecuador, Guayas, Guayaquil, 71); 
+insert into Servicio values(62, 22, "Cuidado de Mascotas", "cuidado bonito", 25.50, Ecuador, Guayas, Guayaquil, 72);
+insert into Servicio values(63, 23, "Cuidado de Mascotas", "cuidado bonito", 25.50, Ecuador, Guayas, Guayaquil, 73); 
+insert into Servicio values(64, 24, "Paseo de Perros", "Paseo entretenido", 30.50, Ecuador, Tarqui, Guayaquil, 74);  
+insert into Servicio values(65, 25, "Paseo de Perros", "Paseo entretenido", 30.50, Ecuador, Tarqui, Guayaquil, 75);
+insert into Servicio values(66, 26, "Paseo de Perros", "Paseo entretenido", 30.50, Ecuador, Tarqui, Guayaquil, 76);
+insert into Servicio values(67, 27, "Alojamiento", "Alojamiento espacioso.", 35.50, Ecuador, Tarqui, Guayaquil, 77);
+insert into Servicio values(68, 28, "Alojamiento", "Alojamiento espacioso.", 35.50, Ecuador, Tarqui, Guayaquil, 78);
+insert into Servicio values(69, 29, "Alojamiento", "Alojamiento espacioso.", 35.50, Ecuador, Tarqui, Guayaquil, 79);
+
+insert into Solicitud values(80, 60, 1, 20, 2023/12/01);
+insert into Solicitud values(81, 61, 2, 21, 2023/12/02);
+insert into Solicitud values(82, 62, 3, 22, 2023/12/03);
+insert into Solicitud values(83, 63, 4, 23, 2023/12/04);
+insert into Solicitud values(84, 64, 5, 24, 2023/12/05);
+insert into Solicitud values(85, 65, 6, 25, 2023/12/06);
+insert into Solicitud values(86, 66, 7, 26, 2023/12/07);
+insert into Solicitud values(87, 67, 8, 27, 2023/12/08);
+insert into Solicitud values(88, 68, 9, 28, 2023/12/09);
+insert into Solicitud values(89, 69, 10, 29, 2023/12/10);
+
+insert into Comentario values(90, 60, 1, 20, 2023/12/02, 10, "Me gusta");
+insert into Comentario values(91, 61, 2, 21, 2023/12/03, 10, "Me gusta");
+insert into Comentario values(92, 62, 3, 22, 2023/12/04, 10, "Me gusta");
+insert into Comentario values(93, 63, 4, 23, 2023/12/05, 10, "Me gusta");
+insert into Comentario values(94, 64, 5, 24, 2023/12/06, 10, "Me gusta");
+insert into Comentario values(95, 65, 6, 25, 2023/12/07, 10, "Me gusta");
+insert into Comentario values(96, 66, 7, 26, 2023/12/08, 10, "Me gusta");
+insert into Comentario values(97, 67, 8, 27, 2023/12/09, 10, "Me gusta");
+insert into Comentario values(98, 68, 9, 28, 2023/12/10, 10, "Me gusta");
+insert into Comentario values(99, 69, 10, 29, 2023/12/11, 10, "Me gusta");
+
