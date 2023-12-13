1ea29cb9196a16560809f2b8b1d83c25e944912b
gennalop <gennalop@espol.edu.ec>
2023-12-12 19:33:08 -0500

    instancias

--- petbacker.sql
+++ petbacker.sql
@@ -5,7 +5,7 @@ create table RefugioMascotas(
 	idRef int not null unique check(idRef > 0 AND idRef < 1000),
     email char(50) not null unique,
     descripcion char(250) not null,
-    foto blob not null,
+    foto char(250) not null,
     pagina_web char(50) not null,
     direccion char(50) not null,
     estado char(30) not null,
@@ -20,7 +20,7 @@ create table Usuario(
     nickname char(250) not null,
     contraseña char(50) not null,
     telefono char(20),
-    foto blob not null,
+    foto char(250) not null,
     red_social char(30),
     vales int not null,
     tarjeta int,
@@ -40,7 +40,7 @@ create table Donacion(
 );
 create table TipoServicio(
 	idTipoSer int not null unique check(idTipoSer > 0 AND idTipoSer < 10),
-    lugar_recogida date not null,
+    lugar_recogida char(250) not null,
     fecha_inicio date not null,
     numero_dias int not null check(numero_dias >= 1 AND numero_dias <= 365),
 	fecha_fin date not null,
@@ -69,7 +69,7 @@ create table TipoMascotas(
 create table TipoAseo(
 	idTipoAseo int not null unique check(idTipoAseo > 0 AND idTipoAseo < 1000000),
     idTipoSer int not null unique check(idTipoSer > 0 AND idTipoSer < 10),
-    aseo char(30) not null check(aseo IN ("básico", "completo", "ducha", "afeitado de almohadillas de patas", "recorte y relleno de uñas")),
+    aseo char(50) not null check(aseo IN ("básico", "completo", "ducha", "afeitado de almohadillas de patas", "recorte y relleno de uñas")),
     primary key(idTipoAseo, idTipoSer),
     foreign key(idtipoSer) references TipoServicio(idTipoSer)
 );
@@ -92,6 +92,7 @@ create table Solicitud(
 	idSer int not null unique check(idSer > 0 AND idSer < 100000),
     idUsu int not null unique check(idUsu > 0 AND idUsu < 100000),
     idCui int not null unique check(idCui > 0 AND idCui < 100000),
+	constraint diferentes1 check(idUsu != idCui),
     fecha date not null,
     primary key(idSol, idSer, idUsu, idCui),
     foreign key(idSer) references Servicio(idSer),
@@ -103,6 +104,7 @@ create table Comentario(
 	idSer int not null unique check(idSer > 0 AND idSer < 100000),
     idUsu int not null unique check(idUsu > 0 AND idUsu < 100000),
     idCui int not null unique check(idCui > 0 AND idCui < 100000),
+	constraint diferentes2 check(idUsu != idCui),
     fecha date not null,
     valoracion double not null,
     contenido char(250) not null,
@@ -110,4 +112,124 @@ create table Comentario(
     foreign key(idSer) references Servicio(idSer),
     foreign key(idUsu) references Usuario(idUsu),
     foreign key(idCui) references Usuario(idUsu)
-)
+);
+
+use PetBacker;
+
+insert into RefugioMascotas values(1, "asd@company.com", "Refugio de Gatitos en el Guasmo",  "https://i0.wp.com/puppis.blog/wp-content/uploads/2022/02/abc-cuidado-de-los-gatos-min.jpg?resize=1024%2C681&ssl=1", "www.gatitosrefugiados.com", "Coop. Unión de bananeros", "Guayas", "Guayaquil", 1234541244, null);
+insert into RefugioMascotas values(2, "das@company.com", "Refugio Perrito", "url.png",  "www.refugioperritos.com", "Portete y la 32", "Guayas", "Guayaquil", 1234541244, null);
+insert into RefugioMascotas values(3, "exc@company.com", "Refugio Animal", "url.png",  "www.refugioanimal.com", "Rumichaca", "Guayas", "Guayaquil", 1234541244, null);
+insert into RefugioMascotas values(4, "def@company.com", "Refugio amiguitos", "url.png",  "www.refugioamigo.com", "Malecón del salado", "Guayas", "Guayaquil", 1234541244, null);
+insert into RefugioMascotas values(5, "qwe@company.com", "Refugio Gato Bonito", "url.png",  "www.refugiogatito.com", "Espol Campus Prosperina", "Guayas", "Guayaquil", 1234541244, null);
+insert into RefugioMascotas values(6, "erd@company.com", "El gato elegante", "url.png",  "www.refugiogatoelegante.com", "Malecón 2000", "Guayas", "Guayaquil", 1234541244, null);
+insert into RefugioMascotas values(7, "wfd@company.com", "Refugio cachorro", "url.png",  "www.refugiocachorro.com", "Mall el Fortín", "Guayas", "Guayaquil", 1234541244, null);
+insert into RefugioMascotas values(8, "gtr@company.com", "Refugio animales seguros", "url.png",  "www.refugioanimalseguro.com", "9 de Octubre", "Guayas", "Guayaquil", 1234541244, null);
+insert into RefugioMascotas values(9, "jtn@company.com", "Refugio Las Peñas", "url.png",  "www.refugiopeñas.com", "Las Peñas", "Guayas", "Guayaquil", 1234541244, null);
+insert into RefugioMascotas values(10, "llm@company.com", "El perro feliz", "url.png",  "www.refugioperrofeli.com", "Urdesa", "Guayas", "Guayaquil", 1234541244, null);
+
+insert into Usuario values(1, "vet@outlook.com", "Vet24", 12345, 3334445551, "image.png", null, 20, 1111111111, null);
+insert into Usuario values(2, "det@outlook.com", "Det41", 54321, 3332245551, "image.jpg", "Det Exa 4", 10, 1111111112, null);
+insert into Usuario values(3, "des@outlook.com", "Des45", "12asd23def", 3334445551, "image.png", null, 35, 1111111113, null);
+insert into Usuario values(4, "rad@outlook.com", "RadIcal", 343451, 3334335551, "image.png", null, 40, 1111111114, null);
+insert into Usuario values(5, "bir@outlook.com", "Birdie21", "Net86", 3334665551, "image.jpg", "Bir Cage 5", 20, 1111111115, null);
+insert into Usuario values(6, "num@outlook.com", "NumLock41", "defaultvalue123", 3224445551, "image.png", null, 20, 1111111116, null);
+insert into Usuario values(7, "cli@outlook.com", "Clitheclient", "ExcellentCompany", 3354445551, "image.png", null, 5, 1111111117, null);
+insert into Usuario values(8, "org@outlook.com", "Org563", "Sparky2024", 3334425651, "image.jpg", null, 0, 1111111118, null);
+insert into Usuario values(9, "pul@outlook.com", "Pulp76", "qw12er34ty5", 3398645551, "image.png", null, 100, 1111111119, null);
+insert into Usuario values(10, "red@outlook.com", "RedlikeaFlower", "qwerty", 3322145551, "image.png", "Red June", 15, 1111111110, null);
+
+insert into Usuario values(20, "der@outlook.com", "Der24", 12345, 3334442551, "image.jpg", "Derek Flint", 0, 1111111101, "Cuidador");
+insert into Usuario values(21, "fer@outlook.com", "Fernando31", 1234, 1334442551, "image.png", "Fernando Pinargote", 40, 1111111102, "Cuidador");
+insert into Usuario values(22, "ter@outlook.com", "Teresse789", "54a631s", 1334443552, "image.png", null, 40, 1111111103, "Cuidador");
+insert into Usuario values(23, "lev@outlook.com", "Levze514", "asd41qwe51.4", 1322442551, "image.png", null, 500, 1111111104, "Cuidador");
+insert into Usuario values(24, "liv@outlook.com", "LivingForever1", "Forever_and.Ever20", 1334442533, "image.png", "Liv Ramos", 0, 1111111105, "Cuidador");
+insert into Usuario values(25, "gru@outlook.com", "GruVillain54", "Moon", 1334242751, "image.png", null, 0, 1111111106, "Cuidador");
+insert into Usuario values(26, "min@outlook.com", "Min0909", "des31ftg.6", 8934442551, "image.png", null, 15, 1111111107, "Cuidador");
+insert into Usuario values(27, "lor@outlook.com", "Lor82", 1234, 1339842551, "image.png", null, 0, 1111111108, "Cuidador");
+insert into Usuario values(28, "gef@outlook.com", "Geff", 54321, 1334476551, "image.png", null, 0, 1111111109, "Cuidador");
+insert into Usuario values(29, "ban@outlook.com", "YouAreBanned", 1285567890, 1334442551, "image.png", "Ban Des", 0, 1111111010, "Cuidador");
+
+insert into TipoServicio values(1, "9 de Octubre", "2023-02-14", 3, "2023-02-17", "Sensible a ruidos", "Siames", 1, "Guardería", false, null);
+insert into TipoServicio values(2, "Alborada", "2023-10-03", 1, "2023-10-04", null, "Mestizo", 2, "Paseo de Perros", true, 2);
+insert into TipoServicio values(3, "Parque Centenario", "2023-12-01", 14, "2023-12-15", null, "Sphynx", 2, "Alojamiento", false, null);
+insert into TipoServicio values(4, "Samborondón", "2023-04-04", 5, "2023-04-09", null, "Pug", 3, "Paseo de Perros", true, 2);
+insert into TipoServicio values(6, "Alborada", "2023-02-03", 20, "2023-02-23", "Es muy jugueton", "Mestizo", 2, "Alojamiento", false, null);
+insert into TipoServicio values(7, "Avenida Kenedy", "2024-02-14", 10, "2024-02-24", "Sensible al frio", "Mestizo", 3, "Alojamiento", true, 3);
+insert into TipoServicio values(8, "Avenida Carchi", "2023-12-24", 1, "2023-12-25", null, "Boxer", 1, "Cuidado de Mascotas", true, 2);
+insert into TipoServicio values(9, "Samborondón", "2023-05-25", 2, "2023-05-27", null, "Mestizo", 2, "Guardería", false, null);
+insert into TipoServicio values(5, "Bella Vista", "2023-07-19", 5, "2023-07-24", "Cuidado lo pisa", "Chihuahua", 2, "Guardería", true, 2);
+
+insert into Tamaño values(1, 1, "1-5kg");
+insert into Tamaño values(2, 2, "5-10kg");
+insert into Tamaño values(3, 3, "1-5kg");
+insert into Tamaño values(4, 4, "5-10kg");
+insert into Tamaño values(5, 5, "1-5kg");
+insert into Tamaño values(6, 6, "20-40kg");
+insert into Tamaño values(7, 7, "5-10kg");
+insert into Tamaño values(8, 8, "20-40kg");
+insert into Tamaño values(9, 9, "5-10kg");
+
+insert into TipoMascotas values (1, 1, "Gato");
+insert into TipoMascotas values (2, 2, "Perro");
+insert into TipoMascotas values (3, 3, "Gato");
+insert into TipoMascotas values (4, 4, "Perro");
+insert into TipoMascotas values (5, 5, "Perro");
+insert into TipoMascotas values (6, 6, "Reptil");
+insert into TipoMascotas values (7, 7, "Perro");
+insert into TipoMascotas values (8, 8, "Perro");
+insert into TipoMascotas values (9, 9, "Otros");
+
+insert into TipoAseo values (1, 1, "completo");
+insert into TipoAseo values (2, 2, "ducha");
+insert into TipoAseo values (3, 3, "ducha");
+insert into TipoAseo values (4, 4, "afeitado de almohadillas de patas");
+insert into TipoAseo values (5, 5, "básico");
+insert into TipoAseo values (6, 6, "ducha");
+insert into TipoAseo values (7, 7, "afeitado de almohadillas de patas");
+insert into TipoAseo values (8, 8, "básico");
+insert into TipoAseo values (9, 9, "recorte y relleno de uñas");
+
+insert into Donacion values(1, 1, 1, "2023-07-24", 5, 10);
+insert into Donacion values(2, 4, 2, "2023-07-24", 5, 10);
+insert into Donacion values(3, 3, 4, "2023-07-24", 5, 10);
+insert into Donacion values(4, 2, 3, "2023-05-25", 20, 40);
+insert into Donacion values(5, 5, 6, "2023-05-25", 25, 50);
+insert into Donacion values(6, 7, 25, "2023-02-12", 50, 100);
+insert into Donacion values(7, 6, 23, "2023-08-03", 1, 2);
+insert into Donacion values(8, 8, 26, "2023-08-04", 1, 2);
+insert into Donacion values(9, 10, 5, "2023-07-24", 30, 60);
+insert into Donacion values(10, 9, 8, "2023-04-24", 12, 24);
+
+insert into Servicio values(60, 20, "Cuidado de Mascotas", "cuidado bonito", 25.50, "Ecuador", "Pichincha", "Quito", 1); 
+insert into Servicio values(61, 21, "Cuidado de Mascotas", "cuidado bonito", 25.50, "Ecuador", "Pichincha", "Quito", 2); 
+insert into Servicio values(62, 22, "Cuidado de Mascotas", "cuidado bonito", 25.50, "Ecuador", "Pichincha", "Quito", 3);
+insert into Servicio values(63, 23, "Cuidado de Mascotas", "cuidado bonito", 25.50, "Ecuador", "Pichincha", "Quito", 4); 
+insert into Servicio values(64, 24, "Paseo de Perros", "Paseo entretenido", 30.50, "Ecuador", "Tarqui", "Guayaquil", 5);  
+insert into Servicio values(65, 25, "Paseo de Perros", "Paseo entretenido", 30.50, "Ecuador", "Tarqui", "Guayaquil", 6);
+insert into Servicio values(66, 26, "Paseo de Perros", "Paseo entretenido", 30.50, "Ecuador", "Tarqui", "Guayaquil", 7);
+insert into Servicio values(67, 27, "Alojamiento", "Alojamiento espacioso.", 35.50, "Ecuador", "Chimborazo", "Riobamba", 8);
+insert into Servicio values(68, 28, "Alojamiento", "Alojamiento espacioso.", 35.50, "Ecuador", "Chimborazo", "Riobamba", 9);
+insert into Servicio values(69, 29, "Alojamiento", "Alojamiento espacioso.", 35.50, "Ecuador", "Chimborazo", "Riobamba", 9);
+
+insert into Solicitud values(80, 60, 1, 20, 2023/12/01);
+insert into Solicitud values(81, 61, 2, 21, 2023/12/02);
+insert into Solicitud values(82, 62, 3, 22, 2023/12/03);
+insert into Solicitud values(83, 63, 4, 23, 2023/12/04);
+insert into Solicitud values(84, 64, 5, 24, 2023/12/05);
+insert into Solicitud values(85, 65, 6, 25, 2023/12/06);
+insert into Solicitud values(86, 66, 7, 26, 2023/12/07);
+insert into Solicitud values(87, 67, 8, 27, 2023/12/08);
+insert into Solicitud values(88, 68, 9, 28, 2023/12/09);
+insert into Solicitud values(89, 69, 10, 29, 2023/12/10);
+
+insert into Comentario values(90, 60, 1, 20, 2023/12/02, 10, "Me gusta");
+insert into Comentario values(91, 61, 2, 21, 2023/12/03, 10, "Me gusta");
+insert into Comentario values(92, 62, 3, 22, 2023/12/04, 10, "Me gusta");
+insert into Comentario values(93, 63, 4, 23, 2023/12/05, 10, "Me gusta");
+insert into Comentario values(94, 64, 5, 24, 2023/12/06, 10, "Me gusta");
+insert into Comentario values(95, 65, 6, 25, 2023/12/07, 10, "Me gusta");
+insert into Comentario values(96, 66, 7, 26, 2023/12/08, 10, "Me gusta");
+insert into Comentario values(97, 67, 8, 27, 2023/12/09, 10, "Me gusta");
+insert into Comentario values(98, 68, 9, 28, 2023/12/10, 10, "Me gusta");
+insert into Comentario values(99, 69, 10, 29, 2023/12/11, 10, "Me gusta");
+
