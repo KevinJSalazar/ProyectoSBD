8b387e8daca4c0b8cdd8b73508cfb5c070d8cdcd
KevinJSalazar <kejosala@espol.edu.ec>
2024-01-05 22:40:25 -0500

    Proyecto código fuente

--- PetBackerSBD/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module ec.edu.espol.petbackersbd {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens ec.edu.espol.petbackersbd to javafx.fxml;
-    exports ec.edu.espol.petbackersbd;
-}
